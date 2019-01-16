d75f8adb64d66d686339cb81559bf24885b3a0c3
Roxana <roxanavp87@gmail.com>
2019-01-15 18:13:46 -0600

    prettify json output

--- csvTojson/csvTojson.js
+++ csvTojson/csvTojson.js
@@ -10,6 +10,6 @@ csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
     console.log(jsonObj);
-    fs.writeFileSync(path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj))
+    fs.writeFileSync(path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj, null, 2))
 })
  

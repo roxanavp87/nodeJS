7c0f8fe2530c22f4fd728eff3648ae7ac9e050cf
Roxana <roxanavp87@gmail.com>
2019-01-16 11:37:04 -0600

    middleware using express

---
+++ middleware/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+// simple middleware
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.url}`);
+    next();
+});
+
+// middleware to check authkey
+// to test in terminal use: curl localhost:3000/?authkey='1234' -i
+app.use((req, res, next) => {
+    if (req.query.authkey) {
+        console.log('user has authorization');
+        next();
+    } else {
+        res.status(401).send('Not authorized');
+
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('home \n');
+});
+
+app.get('/user', (req, res) => {
+    res.send('user \n');
+});
+
+app.listen(3000);
+

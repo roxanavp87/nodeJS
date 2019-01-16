9d03d51ffd3d78d574a3192e0d5bbecbd0b16c80
Roxana <roxanavp87@gmail.com>
2019-01-15 18:21:51 -0600

    catch error in csvtojson convertion

--- csvTojson/csvTojson.js
+++ csvTojson/csvTojson.js
@@ -8,8 +8,15 @@ const csvFilePath=path.join(__dirname, fileName);
 // console.log(csvFilePath)
 csv()
 .fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
+.then( (jsonObj) => {
+    // console.log(jsonObj);
     fs.writeFileSync(path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj, null, 2))
+    console.log('File has been successfully converted')
+    process.exit(0);
 })
+.catch( (error) => {
+    console.log(error);
+    process.exit(1);
+});
+
  

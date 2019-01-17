6e6364db999ccb1ca8674e4da8470651395f3b4e
Roxana <roxanavp87@gmail.com>
2019-01-17 14:17:53 -0600

    update a post

--- Express/blog-api/routes/posts.js
+++ Express/blog-api/routes/posts.js
@@ -3,28 +3,49 @@ const { check, validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 let posts = require('../data/posts.json');
 
-
+// get posts
 app.get('/posts', (req, res) => {
-    res.status(200).send(posts);
+    res.send(posts);
 });
 
-app.post('/posts',[
+// add a new post
+app.post('/posts',[ 
     check('name').trim().isLength({ min: 3 }),
     check('url').trim().isURL(),
-    check('text').trim().not().isEmpty(),
-    sanitizeBody('notifyOnReply').toBoolean()], (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+    check('text').trim().not().isEmpty()],
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        let newPost = {
+            name: req.body.name,
+            url: req.body.url,
+            text: req.body.text,
+            comments: []
+        };
+        let id = posts.length;
+        posts.push(newPost);
+        res.status(201).send({id: id});
     }
-    let newPost = {
-        name: req.body.name,
-        url: req.body.url,
-        text: req.body.text,
-        comments: req.body.comments
-    };
-    let id = posts.length;
-    posts.push(newPost);
-    res.status(201).send({id: id});
-});
+);
+
+// update a post
+app.put('/posts/:id', [ 
+    check('name').trim().isLength({ min: 3 }),
+    check('url').trim().isURL(),
+    check('text').trim().not().isEmpty(),],
+    (req, res) => {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        const id = req.params.id;
+        posts[id].name = req.body.name;
+        posts[id].url = req.body.url;
+        posts[id].text = req.body.text
+        res.send(posts[id]);
+    }
+);

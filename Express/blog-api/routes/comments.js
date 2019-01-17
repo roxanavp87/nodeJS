da84b0a174d1ede29ffb8c6c0260eedc81030753
Roxana <roxanavp87@gmail.com>
2019-01-17 14:44:24 -0600

    add a new comment

--- Express/blog-api/routes/comments.js
+++ Express/blog-api/routes/comments.js
@@ -9,28 +9,23 @@ app.get('/posts/:postId/comments', (req, res) => {
     res.send(comments);
 });
 
-// // add a new post
-// app.post('/posts',[ 
-//     check('name').trim().isLength({ min: 3 }),
-//     check('url').trim().isURL(),
-//     check('text').trim().not().isEmpty()],
-//     (req, res) => {
-//         // Finds the validation errors in this request and wraps them in an object with handy functions
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({ errors: errors.array() });
-//         }
-//         let newPost = {
-//             name: req.body.name,
-//             url: req.body.url,
-//             text: req.body.text,
-//             comments: []
-//         };
-//         let id = posts.length;
-//         posts.push(newPost);
-//         res.status(201).send({id: id});
-//     }
-// );
+// add a new comment
+app.post('/posts/:postId/comments',[ 
+    check('text').trim().not().isEmpty()],
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        let newComment = {
+            text: req.body.text
+        };
+        let id = posts[req.params.postId].comments.length;
+        posts[req.params.postId].comments.push(newComment);
+        res.status(201).send({id: id});
+    }
+);
 
 // // update a post
 // app.put('/posts/:id', [ 
